3666640cd923084f07328d4a5e7b8713f0bf32c2
Ebubechi123 <ebube2003nice@gmail.com>
2021-12-09 15:15:49 +0100

    Firts Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let col = document.querySelector(".col_4");
+
+let description = document.querySelector(".text");
+
+let dataset = [
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"ello"
+    },
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"Deep Earth"
+    },
+    {
+        description:"De Earth"
+    },
+
+]
+
+
+
+
+dataset.forEach((data)=>{
+    description
+})
